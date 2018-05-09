26bc8ed6e4802be0767b29d65c860fe159bc5afa
gblordsburg <bluecoast86@gmail.com>
2018-05-09 15:50:51 -0700

    Add followed_posts function in User model

--- app/models.py
+++ app/models.py
@@ -64,6 +64,12 @@ class User(UserMixin, db.Model):
         gardens = self.gardens
         return gardens
 
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
     def followed_plants(self):
         followed = Plant.query.join(
