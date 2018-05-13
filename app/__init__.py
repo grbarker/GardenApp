79365b686d829d8d441916c7a35a47cc4fce3382
gblordsburg <bluecoast86@gmail.com>
2018-05-12 19:17:26 -0700

    Add flask-moment import

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -12,6 +13,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.init_app(app)
