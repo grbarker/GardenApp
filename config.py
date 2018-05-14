05a5e73077d00b8a521f61f8472f6107bb8339b7
gblordsburg <bluecoast86@gmail.com>
2018-05-14 16:10:50 -0700

    Add email account in config.py and email framework in email.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #sending errors by email
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or smtp.googlemail.com
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or flaskapperrors
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')or wgswtrq9gs
     ADMINS = ['your-email@example.com']
     POSTS_PER_PAGE = 10
     PLANTS_PER_PAGE = 10
