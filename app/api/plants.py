d2ccff8c1c9c61babdc13a43e296ed7a7cb9ec57
gblordsburg <bluecoast86@gmail.com>
2018-10-23 15:35:14 -0700

    Edit how a user's plants are obtained. Removes need for id in url. Adds api endpoint to obtain all of the plants in the db for use with react-native-maps(May/will be changed/removed in the future).

--- app/api/plants.py
+++ app/api/plants.py
@@ -2,7 +2,7 @@ from app import db
 from app.api import bp
 from app.api.errors import bad_request
 from app.api.auth import token_auth
-from flask import jsonify, request, url_for
+from flask import jsonify, request, url_for, g
 from app.models import Post, Plant, User
 
 
@@ -15,10 +15,19 @@ def get_plants():
     data = Plant.to_collection_dict(Plant.query, page, per_page, 'api.get_plants')
     return jsonify(data)
 
-
-@bp.route('/users/<int:id>/plants', methods=['GET'])
+@bp.route('/plants/map', methods=['GET'])
 @token_auth.login_required
-def get_plants_by_user(id):
+def get_all_plants_for_maps():
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10000, type=int), 100)
+    data = Plant.to_collection_dict(Plant.query, page, per_page, 'api.get_all_plants_for_maps')
+    return jsonify(data)
+
+
+@bp.route('/user/plants', methods=['GET'])
+@token_auth.login_required
+def get_plants_by_user():
+    id = g.current_user.id
     user = User.query.get_or_404(id)
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', 10, type=int), 100)
