8c0bf7b641e4a51194b2f7af6ac72744e3533626
grbarker <bluecoast86@gmail.com>
2019-06-11 16:42:55 -0700

    Change config file to get environment variables.

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-might-guess-sometime'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
     #sending errors by email
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.googlemail.com'
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or 1
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'flaskapperrors'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')or 'wgswtrq9gs'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['flaskapperrors@gmail.com']
     POSTS_PER_PAGE = 8
     PLANTS_PER_PAGE = 8
-    GOOGLEMAPS_KEY = os.environ.get('GOOGLEMAPS_KEY') or 'AIzaSyCyX0uZDxs4ekWQz-uSuhvhpABMOFf8QfI'
+    GOOGLEMAPS_KEY_ALL = os.environ.get('GOOGLEMAPS_GEOCODE_KEY')
